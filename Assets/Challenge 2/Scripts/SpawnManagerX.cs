bd3ddbb7b84250ab82bd80fb1c4376d88c5790b7
Mephilos <sth4628@hotmail.com>
2025-04-23 12:11:12 +0900

    Prototype2+Challenge2 끝

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 3.0f;
+    private float spawnInterval = 4.0f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnInterval = Random.Range(3.0f, 5.0f);
+        Debug.Log("스폰 타임: " + spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int ballRandomIndex = Random.Range(0, ballPrefabs.Length);
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballRandomIndex], spawnPos, ballPrefabs[ballRandomIndex].transform.rotation);
+        Debug.Log(ballRandomIndex);
+    }
+
+}
